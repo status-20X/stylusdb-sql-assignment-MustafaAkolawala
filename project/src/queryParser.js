59491ef3061f69a3fec47ca730f6129cea4a28f4
MustafaAkolawala <mustafaakolawala@eng.rizvi.edu.in>
2024-04-06 21:31:02 +0530

    feat: Assignment 6

--- project/src/queryParser.js
+++ project/src/queryParser.js
@@ -1,17 +1,37 @@
 function parseQuery(query) {
+  try {
     const selectRegex = /SELECT (.+?) FROM (.+?)(?: WHERE (.*))?$/i;
     const match = query.match(selectRegex);
 
     if (match) {
-        const [, fields, table, whereClause] = match;
-        return {
-            fields: fields.split(',').map(field => field.trim()),
-            table: table.trim(),
-            whereClause: whereClause ? whereClause.trim() : null
-        };
+      const [, fields, table, whereString] = match;
+      const whereClauses = whereString ? parseWhereClause(whereString) : [];
+      return {
+        fields: fields.split(",").map((field) => field.trim()),
+        table: table.trim(),
+        whereClauses,
+      };
     } else {
-        throw new Error('Invalid query format');
+      throw new Error("Invalid query format");
     }
+  } catch (error) {
+    console.error("Error parsing the query", error.message);
+    throw error;
+  }
 }
+
+function parseWhereClause(whereString) {
+  try {
+    const conditions = whereString.split(/ AND | OR /i);
+    return conditions.map((condition) => {
+      const [field, operator, value] = condition.split(/\s+/);
+      return { field, operator, value };
+    });
+  } catch (error) {
+    console.error("Error parsing the Where clause", error.message);
+    throw error;
+  }
+}
+
 
 module.exports = parseQuery;
